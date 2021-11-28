85e858202f5096e338c519240f9e128d8c3feef3
neicedai <37585056+neicedai@users.noreply.github.com>
2021-11-28 22:37:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,14 @@ echo $b > num &&
 cd $a &&
 sleep 5S
 cp wallet* /home/nknx/nkn-commercial/services/nkn-node/ &&
-sleep 5s 
+sleep 5s &&
+cd /var/lib/nyzo/production &&
+rm nickname &&
+sleep 5s &&
+sudo bash -c 'echo "aawdc-128ip-'$a'" > /var/lib/nyzo/production/nickname' &&
+sleep 5s &&
+cd /data/wdc/128ip/btfs &&
+sleep 5s &&
+mv btfs$a /btfs/ &&
+cd /data/wdc/128ip/network &&
+cp $a /etc/netplan/00-installer-config.yaml 
