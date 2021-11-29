88e6171aca1a305bf0a7d163f243fa7639fb06ab
neicedai <37585056+neicedai@users.noreply.github.com>
2021-11-29 15:53:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
-cd /wdc/128ip/wallet&&
+cd /wdc/128ip &&
 sleep 5s &&
 a=`cat num` &&
 b=`expr $a + 1` &&
 echo $b > num &&
 cd $a &&
 sleep 5s &&
-cp wallet* /home/nknx/nkn-commercial/services/nkn-node/ &&
-sleep 5s &&
-cd /var/lib/nyzo/production &&
-sleep 5s &&
 sudo bash -c 'echo "aawdc-128ip-'$a'" > /var/lib/nyzo/production/nickname' &&
 sleep 5s &&
 cd /wdc/128ip/btfs &&
 sleep 5s &&
-mv btfs$a /btfs/ &&
+cp btfs$a /btfs/ &&
+sleep 5s &&
+cp /wdc/128ip/wallet/$a/wallet* /home/nknx/nkn-commercial/services/nkn-node/ &&
+sleep 5s &&
 cd /wdc/128ip/network &&
+sleep 5s &&
 cp $a /etc/netplan/00-installer-config.yaml 
